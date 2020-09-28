5da6df9f57725486d1eb8376701c83c86269aed6
dixitvora <dixitvora@gmail.com>
2020-09-28 20:24:27 +0530

    Updates

---
+++ precache-manifest.d623af67336daeb96ae0824d4179c05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2879c4aeebb31a6fd7a2df469eb35c9",
+    "url": "/monster-box/index.html"
+  },
+  {
+    "revision": "2f5435a23af5339f90f1",
+    "url": "/monster-box/static/css/main.534139ef.chunk.css"
+  },
+  {
+    "revision": "19714b6e8cf667a23533",
+    "url": "/monster-box/static/js/2.df55acea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-box/static/js/2.df55acea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5435a23af5339f90f1",
+    "url": "/monster-box/static/js/main.0613d885.chunk.js"
+  },
+  {
+    "revision": "25ae0a6a690489fa1221",
+    "url": "/monster-box/static/js/runtime-main.b0b2034d.js"
+  }
+]);
