b9ef321b7f86b1102f38816ceba33623ad219232
dixitvora <dixitvora@gmail.com>
2020-09-28 20:29:22 +0530

    Chore:change the h1 in cards

--- src/component/card/Card.component.jsx
+++ src/component/card/Card.component.jsx
@@ -5,7 +5,7 @@ export const Card = ({ monster }) => {
   return (
     <div className='card-container'>
       <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt='' />
-      <h1>{monster.name} </h1>
+      <h3>{monster.name} </h3>
       <p>{monster.email}</p>
     </div>
   );
